b798d7537dff82d8930d8b18836ca97b03f9dbf0
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2025-03-06 01:33:49 +0100

    test(flex-layout): migrate custom matchers to Vitest

--- projects/libs/flex-layout/test-setup.ts
+++ projects/libs/flex-layout/test-setup.ts
@@ -1,6 +1,7 @@
 import '@angular/compiler';
 import '@analogjs/vitest-angular/setup-zone';
 import '@testing-library/jest-dom/vitest';
+import './_private-utils/testing/custom-matchers';
 
 import { getTestBed } from '@angular/core/testing';
 import {
